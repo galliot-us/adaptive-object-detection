83ae312994a605c5e55f57403597c69c5bcac354
alpha-carinae29 <alpha.carinae29@gmail.com>
2021-01-17 19:47:37 +0000

    add detector's base class

---
+++ detectors/base_detector.py
@@ -0,0 +1,32 @@
+import abc
+
+
+class BaseDetector(abc.ABC):
+    """
+    A base class for detectors.
+    the following should be overridden:
+    load_model()
+    preprocess()
+    inference()
+    """
+    def __init__(self, width, height, thresh):
+        self.width = width
+        self.height = height
+        self.thresh = thresh
+        self.model = None
+
+    @abc.abstractmethod
+    def load_model(self, model_path):
+        raise NotImplementedError
+
+ 
+    @abc.abstractmethod
+    def preprocess(self, raw_image):
+        raise NotImplementedError
+
+ 
+    @abc.abstractmethod
+    def inference(self, preprocessed_image):
+        raise NotImplementedError
+
+  
