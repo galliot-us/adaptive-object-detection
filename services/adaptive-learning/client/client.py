7da8a5720286a62011fcc092cec3615f86632f43
mrn-mln <mehraliangit@gmail.com>
2021-01-21 16:57:25 +0330

    added upload file to client

--- services/adaptive-learning/client/client.py
+++ services/adaptive-learning/client/client.py
@@ -1,6 +1,7 @@
 from configs.config_engine import ConfigEngine
 from neuralet_requests import NeuraletRequest
 from argparse import ArgumentParser
+import os
 
 req = NeuraletRequest()
 
@@ -20,6 +21,17 @@ def download_model(task_id):
     req.get_model(task_id)
 
 
+def upload_file(file_path):
+    uploaded_file_name = None
+    if os.path.isfile(file_path):
+        file_name = file_path.split('/')[-1]
+        file = (file_name, open(file_path, 'rb'))
+        uploaded_file_name = req.send_file(file, file_name)
+    else:
+        raise Exception("Invalid Arguments, expected a file that exists not %r" % (file_path))
+    return uploaded_file_name
+
+
 def main():
     argparse = ArgumentParser()
     argparse.add_argument('--config', type=str, help='config file path', default='configs/config-x86.ini')
@@ -27,13 +39,16 @@ def main():
     argparse.add_argument('--task_type', type=int, help=''
                                                         '0: initialize a new adaptive learning request'
                                                         '1: get the status of initialized task'
-                                                        '2: download the trained model',
+                                                        '2: download the trained model'
+                                                        '3: upload file',
                           default=0)
+    argparse.add_argument('--file_path', type=str, help='file path for uploading', default=None)
 
     args = argparse.parse_args()
     config_path = args.config
     task_id = args.task_id
     task_type = args.task_type
+    file_path = args.file_path
 
     if task_type == 0:
         print(f"Task type {task_type}: Initialize a new task.")
@@ -46,6 +61,9 @@ def main():
     elif task_type == 2:
         print(f"Task type {task_type}: Download the trained model from server.")
         download_model(task_id)
+    elif task_type == 3:
+        print(f"Task type {task_type}: Upload file to server.")
+        upload_file(file_path)
     else:
         raise ValueError(f"Task type {task_type}: Is not supported")
 
--- services/adaptive-learning/client/neuralet_requests.py
+++ services/adaptive-learning/client/neuralet_requests.py
@@ -1,12 +1,13 @@
 import json
 import urllib.request
 from pprint import pprint
+import requests
 
 
 class NeuraletRequest():
 
     @staticmethod
-    def send_config():
+    def send_config(config):
         config_json = json.dumps(config.get_section_dict('Teacher')).encode('utf-8')
         headers = {
             "Content-Type": "application/json",
@@ -34,3 +35,19 @@ class NeuraletRequest():
         with urllib.request.urlopen(url) as response:
             html = response.read()
         pprint(html)
+
+    @staticmethod
+    def send_file(file, file_name):
+        url = "https://api.neuralet.io/api/v1/upload/"
+        headers = {
+            "file": str(file_name)
+        }
+        print("Uploading file, please wait it may take a while ...!")
+        r = requests.post(url, headers=headers, files=file)
+        if r.status_code == 200
+            uploaded_file_name = r.json()['filename']
+            print(f'The file is uploaded successfully. Uploaded filename = {uploaded_file_name}')
+            return uploaded_file_name
+        else:
+            print(f'ERROR! ({r.status_code})')
+            return None
