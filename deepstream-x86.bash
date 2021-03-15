c2964579ddef594bf2eee03784fb10afc0ada01a
Undefined References <undefined.references@gmail.com>
2021-03-15 19:38:46 +0330

    tiny fix

--- deepstream-x86.bash
+++ deepstream-x86.bash
@@ -3,7 +3,7 @@
 cd ssd_mobilenet_v2_coco && mkdir -p 1
 cp /repo/deepstream-data/frozen_inference_graph.pb 1/model.graphdef
 
-label_file_path="/repo/ssd_mobilenet_v2_coco/label.txt"
+label_file_path="/repo/ssd_mobilenet_v2_coco/labels.txt"
 if [ ! -f $label_file_path ]; then
     wget https://raw.githubusercontent.com/NVIDIA-AI-IOT/deepstream_triton_model_deploy/master/faster_rcnn_inception_v2/config/labels.txt
 fi
