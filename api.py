5101671f608f54c08d71658ebf87d778feba173c
mrn-mln <mehraliangit@gmail.com>
2021-03-29 07:59:36 -0700

    added api module

---
+++ api.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from io import BytesIO
+import json
+
+from inference_single_img import inference
+
+from fastapi import FastAPI
+from fastapi import UploadFile, File
+
+app = FastAPI()
+
+@app.post("/api/inference")
+async def inference_image(file: UploadFile = File(...)):
+    # read the file
+    rec_file = await file.read()
+    img = Image.open(BytesIO(rec_file))
+    result = inference(img)
+    print(result)
+    return(json.dumps(result))
