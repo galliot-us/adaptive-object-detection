1b7c43cd20f28d0e430931b4dd30c30b46ced984
Undefined References <undefined.references@gmail.com>
2021-05-04 18:16:35 +0430

    deepstream ssd python added, working with ds 5.1

---
+++ deepstream/5.1/deepstream-51-x86.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+videoPath="file://$1"
+
+cd ssd_mobilenet_v2 && mkdir -p 1
+cp /repo/deepstream-data/frozen_inference_graph.pb 1/model.graphdef
+
+label_file_path="/repo/deepstream/5.1/ssd_mobilenet_v2/labels.txt"
+if [ ! -f $label_file_path ]; then
+        wget https://raw.githubusercontent.com/NVIDIA-AI-IOT/deepstream_triton_model_deploy/master/faster_rcnn_inception_v2/config/labels.txt
+fi
+
+cd ..
+python3 deepstream_ssd_parser.py $videoPath $label_file_path
+
