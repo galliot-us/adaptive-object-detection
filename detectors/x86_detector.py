a2a50ec2083009dab1700c2ec05226e75f00c703
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-02 04:12:32 +0000

    Adding tarfile member sanitization to extractall()

--- detectors/x86_detector.py
+++ detectors/x86_detector.py
@@ -34,7 +34,26 @@ class X86Detector(BaseDetector):
                 os.makedirs(base_dir, exist_ok=True)
                 wget.download(base_url + model_file, base_dir)
                 with tarfile.open(base_dir + model_file, "r") as tar:
-                    tar.extractall(path=base_dir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, path=base_dir)
 
         model_dir = pathlib.Path(model_path) / "saved_model"
         model = tf.saved_model.load(str(model_dir))
